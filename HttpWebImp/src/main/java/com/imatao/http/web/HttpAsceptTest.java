7fa340956f86521beca896755a0544e74d404eb5
tao.kaili <tao.kaili@immomo.com>
2019-11-16 18:18:20 +0800

    1

---
+++ HttpWebImp/src/main/java/com/imatao/http/web/HttpAsceptTest.java
@@ -0,0 +1,17 @@
+package com.imatao.http.web;
+
+
+import java.lang.annotation.*;
+
+/**
+ * 这个接口主要是用于给别人用的，作为一个注解的接口，aop编程，
+ */
+
+@Target(ElementType.METHOD) //该注解的作用范围
+
+@Retention(RetentionPolicy.RUNTIME)//描述注解的生命周期，
+
+@Documented
+public @interface HttpAsceptTest {
+
+}
